171a45d13e6f839ed358b7b2e5297ba15fe0a099
finanzero <cynthia.dipoli@gmail.com>
2024-11-04 11:04:08 -0300

    adição de fallback

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import app from "./src/app.js";
 import conectaNoMongo from "./src/config/db.js";
 
-const PORTA = process.env.PORT;
+const PORTA = process.env.PORT || 3000;
 
 app.listen(PORTA, async() => {
     await conectaNoMongo()
+    console.log(`Servidor rodando na porta ${PORTA}`);
 })
