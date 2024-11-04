c166c9772aaac56c2aa91ceb4e231f7240deed6a
finanzero <cynthia.dipoli@gmail.com>
2024-11-04 19:02:00 -0300

    fixes

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const PORTA = process.env.PORT || 3000;
 
 app.listen(PORTA, async () => {
     await conectaNoMongo()
-    routes(app);
     console.log(`Servidor rodando na porta ${PORTA}`);
 })
+
+routes(app);
 
 export default app;
