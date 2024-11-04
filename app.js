c7cf631391ed156f1ee5e45f22fc3be6da2f9c43
finanzero <cynthia.dipoli@gmail.com>
2024-11-04 19:26:34 -0300

    setting up db connection

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import express from "express";
 import routes from "./src/routes/index.js";
-import conectaNoMongo from "./src/config/db.js";
 
 const app = express();
 
 const PORTA = process.env.PORT;
 
 app.listen(PORTA, async () => {
-    await conectaNoMongo()
     console.log(`Servidor rodando na porta ${PORTA}`);
 })
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import ranking from "./ranking.js";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
+import conectaNoMongo from "../config/db.js";
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -14,7 +15,8 @@ const corsOptions = {
     methods: ["GET", "POST"],
 }
 
-const routes = (app) => {
+const routes = async(app) => {
+    await conectaNoMongo()
     app.route("/").get((req, res) => res.status(200).send("Pumpkin Api"));
     app.use(express.json(), ranking, limiter, cors(corsOptions));
 };
