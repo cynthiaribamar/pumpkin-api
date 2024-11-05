10075e6dc5db4a672c079d55be8e8b6104ef72e8
finanzero <cynthia.dipoli@gmail.com>
2024-11-04 23:52:08 -0300

    fixed config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ranking from "./ranking.js";
 import rateLimit from "express-rate-limit";
+import cors from "cors";
 import conectaNoMongo from "../config/db.js";
 
 const limiter = rateLimit({
@@ -9,10 +10,16 @@ const limiter = rateLimit({
     message: "Muitas requisições vindas deste IP. Tente novamente mais tarde."
 })
 
+const corsOptions = {
+    origin: ["https://pumpkin-run.vercel.app/", "https://site-endless-run-game.vercel.app/"],
+    methods: ["GET", "POST"],
+    credentials: true
+}
+
 const routes = async(app) => {
     await conectaNoMongo()
     app.route("/").get((req, res) => res.status(200).send("Pumpkin Api"));
-    app.use(express.json(), ranking, limiter);
+    app.use(express.json(), ranking, limiter, cors(corsOptions));
 };
 
 export default routes;
